0a024b76a403dbe844f9675dfe1ccb0fefeacc6e
Patrícia <patri.b.morais@gmail.com>
2021-10-06 00:15:07 -0300

    feature/adding GET & POST methods

--- src/main/java/com/github/pattrie/todolist/controller/TaskController.java
+++ src/main/java/com/github/pattrie/todolist/controller/TaskController.java
@@ -1,4 +1,36 @@
 package com.github.pattrie.todolist.controller;
 
+import com.github.pattrie.todolist.model.Task;
+import com.github.pattrie.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Task> getAllTasks() {
+        return taskService.listAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> getById(@PathVariable Long id) {
+        return taskService.findById(id);
+    }
 }
--- src/main/java/com/github/pattrie/todolist/repository/TaskRepository.java
+++ src/main/java/com/github/pattrie/todolist/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.github.pattrie.todolist.repository;
 
-public class TaskRepository {
+import com.github.pattrie.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
