8df4cf5415d8648ef140bf87a5aa1ba6c7a0bcb5
Patrícia <patri.b.morais@gmail.com>
2021-11-06 12:53:17 -0300

    [feature] Inserting and setting up Swagger documentation

--- src/main/java/com/github/pattrie/todolist/controller/TaskController.java
+++ src/main/java/com/github/pattrie/todolist/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.github.pattrie.todolist.controller;
 
 import com.github.pattrie.todolist.model.Task;
 import com.github.pattrie.todolist.service.TaskService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +28,11 @@ public class TaskController {
 
   private TaskService taskService;
 
+  @ApiOperation(value = "Creating a new task")
+  @ApiResponses(value = {
+      @ApiResponse(code = 201, message = "Successfully created task"),
+      @ApiResponse(code = 500, message = "There was an error creating the task, check the information")
+  })
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Task createTask(@RequestBody Task task) {
@@ -32,6 +40,11 @@ public class TaskController {
     return taskService.createTask(task);
   }
 
+  @ApiOperation(value = "Listing all tasks")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Successfully listed tasks"),
+      @ApiResponse(code = 500, message = "There was an error listing the tasks, check the information")
+  })
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Task> getAllTasks() {
@@ -39,6 +52,11 @@ public class TaskController {
     return taskService.listAllTasks();
   }
 
+  @ApiOperation(value = "Searching task")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Successfully found task"),
+      @ApiResponse(code = 404, message = "Task not found with this id")
+  })
   @GetMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<Task> getById(@PathVariable Long id) {
@@ -46,6 +64,11 @@ public class TaskController {
     return taskService.findById(id);
   }
 
+  @ApiOperation(value = "Updating task")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Successfully updated task"),
+      @ApiResponse(code = 404, message = "Task not found with this id")
+  })
   @PutMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<Task> updateById(@PathVariable Long id, @RequestBody Task task) {
@@ -53,6 +76,11 @@ public class TaskController {
     return taskService.updateById(task, id);
   }
 
+  @ApiOperation(value = "Deleting task")
+  @ApiResponses(value = {
+      @ApiResponse(code = 204, message = "Successfully deleted task"),
+      @ApiResponse(code = 404, message = "Task not found with this id")
+  })
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public ResponseEntity<Object> deleteById(@PathVariable Long id) {
